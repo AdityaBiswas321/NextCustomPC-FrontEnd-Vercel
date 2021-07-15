2f4cd7a5d1bcdfb2ac623c7ec4ee3faf37e54922
aditya <adityabiswas1999@hotmail.com>
2021-07-15 07:19:08 -0700

    first attempt to seperate frontend and backend, frontend first

---
+++ frontend/next.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  images: {
+    domains: ['secondbucketforcustompc.s3.us-east-2.amazonaws.com'],
+  },
+}
+
+
+
+
+// module.exports = {
+//   images: {
+//     loader: 'imgix',
+//     path: 'secondbucketforcustompc.s3.us-east-2.amazonaws.com'
+//   },
+//   exportPathMap: function () {
+//     return {
+//       '/': { page: '/'}
+//     }
+//   }
+// }
