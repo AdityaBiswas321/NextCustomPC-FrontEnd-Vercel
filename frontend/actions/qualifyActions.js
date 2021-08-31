3c9bdc6043c9bbd86ce7c807376d7402cee15a8d
aditya <adityabiswas1999@hotmail.com>
2021-08-31 00:19:13 -0700

    created actions, and constants, working up changing local state to global state

---
+++ frontend/actions/qualifyActions.js
@@ -0,0 +1,43 @@
+import {
+  STEPONE_CONVERT,
+  STEPTWO_CONVERT,
+  STEPTHREE_CONVERT,
+  STEPFOUR_CONVERT,
+  STEPFIVE_CONVERT,
+} from "../constants/qualifyConstants";
+
+export const stepOne = () => (dispatch) => {
+  try {
+    dispatch({ type: STEPONE_CONVERT });
+  } catch (error) {
+    res.json(error);
+  }
+};
+export const stepTwo = () => (dispatch) => {
+  try {
+    dispatch({ type: STEPTWO_CONVERT });
+  } catch (error) {
+    res.json(error);
+  }
+};
+export const stepThree = () => (dispatch) => {
+  try {
+    dispatch({ type: STEPTHREE_CONVERT });
+  } catch (error) {
+    res.json(error);
+  }
+};
+export const stepFour = () => (dispatch) => {
+  try {
+    dispatch({ type: STEPFOUR_CONVERT });
+  } catch (error) {
+    res.json(error);
+  }
+};
+export const stepFive = () => (dispatch) => {
+  try {
+    dispatch({ type: STEPFIVE_CONVERT });
+  } catch (error) {
+    res.json(error);
+  }
+};
