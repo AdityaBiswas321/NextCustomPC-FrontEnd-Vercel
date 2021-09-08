09c03765e10aa625d8420cc25454a29967443f57
aditya <adityabiswas1999@hotmail.com>
2021-09-07 23:55:07 -0700

    initialized live chat api, require dynamic module due to SSR

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,6 +1,9 @@
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
+import Script from "next/script";
+import dynamic from "next/dynamic";
+import LiveChat from "react-livechat";
 
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 
@@ -23,55 +26,80 @@ export default function HomeScreen() {
 
     console.log("button worked");
   };
+  const DynamicComponentWithNoSSR = dynamic(() => import("react-livechat"), {
+    ssr: false,
+  });
 
   return (
     <>
-      <Row className="w">
-        <Col id="caro" sm={12} md={12} lg={6} xl={6}>
-          <Carousel showStatus={false} showThumbs={false}>
-            <div>
-              <img src="https://secondbucketforcustompc.s3.us-east-2.amazonaws.com/playstation.jpg" />
-            </div>
-            <div>
-              <img src="https://secondbucketforcustompc.s3.us-east-2.amazonaws.com/playstation.jpg" />
-            </div>
-            <div>
-              <img src="https://secondbucketforcustompc.s3.us-east-2.amazonaws.com/playstation.jpg" />
-            </div>
-          </Carousel>
-        </Col>
-      </Row>
+      <>
+        <Row className="w">
+          <Col id="caro" sm={12} md={12} lg={6} xl={6}>
+            <Carousel showStatus={false} showThumbs={false}>
+              <div>
+                <img src="https://secondbucketforcustompc.s3.us-east-2.amazonaws.com/playstation.jpg" />
+              </div>
+              <div>
+                <img src="https://secondbucketforcustompc.s3.us-east-2.amazonaws.com/playstation.jpg" />
+              </div>
+              <div>
+                <img src="https://secondbucketforcustompc.s3.us-east-2.amazonaws.com/playstation.jpg" />
+              </div>
+            </Carousel>
+            <DynamicComponentWithNoSSR license={"13099728"} />
+          </Col>
+        </Row>
 
-      <Row>
-        <Col>
-          <Card>
-            <Card.Body>
-              <Card.Text id="bh">
-                Tailor Made Computer Designs <br></br>Products, Specification,
-                and Installation <br></br>Exact Quotation in Writing and
-                Gauranteed for 30 days <br></br>
-                <Link href="computer1">
-                  <a>
-                    <u className="hello">Book a no obligation quotation</u>
-                  </a>
-                </Link>
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
+        <Row>
+          <Col>
+            <Card>
+              <Card.Body>
+                <Card.Text id="bh">
+                  Tailor Made Computer Designs <br></br>Products, Specification,
+                  and Installation <br></br>Exact Quotation in Writing and
+                  Gauranteed for 30 days <br></br>
+                  <Link href="computer1">
+                    <a>
+                      <u className="hello">Book a no obligation quotation</u>
+                    </a>
+                  </Link>
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
 
-      <Row id="computer">
-        <Col sm={12} md={6} lg={4} xl={4}>
-          <Card id="gh" type="button" className="btn btn-dark ">
-            <Link href="computer1">
-              <a>
-                <span>Computer1</span>
-              </a>
-            </Link>
-          </Card>
-          <Card>
-            <Link href="computer1">
+        <Row id="computer">
+          <Col sm={12} md={6} lg={4} xl={4}>
+            <Card id="gh" type="button" className="btn btn-dark ">
+              <Link href="computer1">
+                <a>
+                  <span>Computer1</span>
+                </a>
+              </Link>
+            </Card>
+            <Card>
+              <Link href="computer1">
+                <Image
+                  src={
+                    "https://secondbucketforcustompc.s3.us-east-2.amazonaws.com/playstation.jpg"
+                  }
+                  width={25}
+                  height={25}
+                  layout="responsive"
+                />
+              </Link>
+            </Card>
+          </Col>
+          <Col sm={12} md={6} lg={4} xl={4}>
+            <Card id="gh" type="button" className="btn btn-dark ">
+              <Link href="computer1">
+                <a>
+                  <span>Computer1</span>
+                </a>
+              </Link>
+            </Card>
+            <Card>
               <Image
                 src={
                   "https://secondbucketforcustompc.s3.us-east-2.amazonaws.com/playstation.jpg"
@@ -80,48 +108,29 @@ export default function HomeScreen() {
                 height={25}
                 layout="responsive"
               />
-            </Link>
-          </Card>
-        </Col>
-        <Col sm={12} md={6} lg={4} xl={4}>
-          <Card id="gh" type="button" className="btn btn-dark ">
-            <Link href="computer1">
-              <a>
-                <span>Computer1</span>
-              </a>
-            </Link>
-          </Card>
-          <Card>
-            <Image
-              src={
-                "https://secondbucketforcustompc.s3.us-east-2.amazonaws.com/playstation.jpg"
-              }
-              width={25}
-              height={25}
-              layout="responsive"
-            />
-          </Card>
-        </Col>
-        <Col sm={12} md={6} lg={4} xl={4}>
-          <Card id="gh" type="button" className="btn btn-dark ">
-            <Link href="computer1">
-              <a>
-                <span>Computer1</span>
-              </a>
-            </Link>
-          </Card>
-          <Card>
-            <Image
-              src={
-                "https://secondbucketforcustompc.s3.us-east-2.amazonaws.com/playstation.jpg"
-              }
-              width={25}
-              height={25}
-              layout="responsive"
-            />
-          </Card>
-        </Col>
-      </Row>
+            </Card>
+          </Col>
+          <Col sm={12} md={6} lg={4} xl={4}>
+            <Card id="gh" type="button" className="btn btn-dark ">
+              <Link href="computer1">
+                <a>
+                  <span>Computer1</span>
+                </a>
+              </Link>
+            </Card>
+            <Card>
+              <Image
+                src={
+                  "https://secondbucketforcustompc.s3.us-east-2.amazonaws.com/playstation.jpg"
+                }
+                width={25}
+                height={25}
+                layout="responsive"
+              />
+            </Card>
+          </Col>
+        </Row>
+      </>
     </>
   );
 }
