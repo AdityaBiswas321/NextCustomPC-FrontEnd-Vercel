20195a18b2a3681938ed3c027d8f2c2cdffe1cf1
aditya <adityabiswas1999@hotmail.com>
2021-10-09 17:29:00 -0700

    add useEffect Import

--- frontend/pages/product.js
+++ frontend/pages/product.js
@@ -1,4 +1,4 @@
-import React from "react";
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import Image from "next/image";
 import Product from "../components/Product/Product";
