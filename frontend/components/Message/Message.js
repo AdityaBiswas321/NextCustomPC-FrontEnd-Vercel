8a56960c9f6ee291c4187f56b9c1e579bc7f38f9
aditya <adityabiswas1999@hotmail.com>
2021-11-20 03:08:55 -0800

    ui chane to message

--- frontend/components/Message/Message.js
+++ frontend/components/Message/Message.js
@@ -3,7 +3,7 @@ import { Alert } from "react-bootstrap";
 
 const Message = ({ variant, children }) => {
   return (
-    <Alert variant={variant} className="text-center">
+    <Alert variant={variant} className="text-center thumb">
       {children}
     </Alert>
   );
