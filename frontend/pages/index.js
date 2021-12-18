6ca475243e456e350ad0bf43d0dfff8ff68ee775
aditya <adityabiswas1999@hotmail.com>
2021-12-18 06:09:01 -0800

    Update sizing
    original didn't work

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -7,7 +7,7 @@ import LiveChat from "react-livechat";
 
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 
-import { Row, Col, Button, Card, Navbar } from "react-bootstrap";
+import { Row, Col, Button, Card, Navbar, Container } from "react-bootstrap";
 import { Carousel } from "react-responsive-carousel";
 
 import { useDispatch } from "react-redux";
@@ -32,132 +32,130 @@ import { Motion } from "react-motion";
 export default function HomeScreen() {
   return (
     <div className="lightgrey">
-      <Card className="thumbIndex darkgrey">
-        <Card className="">
-          <Row className="w">
-            <Col id="caro">
-              <Carousel showStatus={false} showThumbs={false}>
-                <div>
-                  <Image
-                    src={
-                      "https://secondbucketforcustompc.s3.us-east-2.amazonaws.com/TestPC1.png"
-                    }
-                    width={25}
-                    height={25}
-                    layout="responsive"
-                  />
-                </div>
-                <div>
-                  <Image
-                    src={
-                      "https://secondbucketforcustompc.s3.us-east-2.amazonaws.com/WhitePC.jpeg"
-                    }
-                    width={25}
-                    height={25}
-                    layout="responsive"
-                  />
-                </div>
-                <div>
-                  <Image
-                    src={
-                      "https://secondbucketforcustompc.s3.us-east-2.amazonaws.com/BluePc.jpg"
-                    }
-                    width={25}
-                    height={25}
-                    layout="responsive"
-                  />
-                </div>
-              </Carousel>
-            </Col>
-          </Row>
+      <Col className="thumbIndex" xl={4} lg={4} style={{ padding: "0" }}>
+        <Row className="w">
+          <Col id="caro">
+            <Carousel showStatus={false} showThumbs={false}>
+              <div>
+                <Image
+                  src={
+                    "https://secondbucketforcustompc.s3.us-east-2.amazonaws.com/TestPC1.png"
+                  }
+                  width={25}
+                  height={25}
+                  layout="responsive"
+                />
+              </div>
+              <div>
+                <Image
+                  src={
+                    "https://secondbucketforcustompc.s3.us-east-2.amazonaws.com/WhitePC.jpeg"
+                  }
+                  width={25}
+                  height={25}
+                  layout="responsive"
+                />
+              </div>
+              <div>
+                <Image
+                  src={
+                    "https://secondbucketforcustompc.s3.us-east-2.amazonaws.com/BluePc.jpg"
+                  }
+                  width={25}
+                  height={25}
+                  layout="responsive"
+                />
+              </div>
+            </Carousel>
+          </Col>
+        </Row>
 
-          <Row>
-            <Col>
-              <Card className="mb-2 mt-2">
-                <Card.Body>
-                  <Card.Text id="bh">
-                    Tailor Made Computer Designs <br></br>Products,
-                    Specification, and Installation <br></br>Exact Quotation in
-                    Writing and Gauranteed for 30 days <br></br>
-                    <Link href="computer1">
-                      <a>
-                        <u className="hello">Book a no obligation quotation</u>
-                      </a>
-                    </Link>
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-            </Col>
-          </Row>
+        <Row>
+          <Col>
+            <Card className="mb-2 mt-2">
+              <Card.Body>
+                <Card.Text id="bh">
+                  Tailor Made Computer Designs <br></br>Products, Specification,
+                  and Installation <br></br>Exact Quotation in Writing and
+                  Gauranteed for 30 days <br></br>
+                  <Link href="computer1">
+                    <a>
+                      <u className="hello">Book a no obligation quotation</u>
+                    </a>
+                  </Link>
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
 
-          <Row id="computer">
-            <Col sm={12} md={12} lg={12} xl={12}>
-              <Card id="gh" type="button" className="btn btn-dark ">
-                <Link href="computer1">
-                  <a>
-                    <span>View Products</span>
-                  </a>
-                </Link>
-              </Card>
-              <Card>
-                <Link href="computer1">
-                  <Image
-                    src={
-                      "https://secondbucketforcustompc.s3.us-east-2.amazonaws.com/TestPC1.png"
-                    }
-                    width={25}
-                    height={25}
-                    layout="responsive"
-                  />
-                </Link>
-              </Card>
-            </Col>
+        <Row id="computer">
+          <Col sm={12} md={12} lg={12} xl={12} style={{ padding: "0" }}>
+            <Card id="gh" type="button" className="btn btn-dark ">
+              <Link href="computer1">
+                <a>
+                  <span>View Products</span>
+                </a>
+              </Link>
+            </Card>
+            <Card>
+              <Link href="computer1">
+                <Image
+                  src={
+                    "https://secondbucketforcustompc.s3.us-east-2.amazonaws.com/TestPC1.png"
+                  }
+                  width={25}
+                  height={25}
+                  layout="responsive"
+                />
+              </Link>
+            </Card>
+          </Col>
 
-            <Col sm={12} md={12} lg={12} xl={12}>
-              <Card id="gh" type="button" className="btn btn-dark ">
-                <Link href="computer1">
-                  <a>
-                    <span>About Us</span>
-                  </a>
-                </Link>
-              </Card>
-              <Card>
-                <Link href="computer1">
-                  <Image
-                    src={
-                      "https://secondbucketforcustompc.s3.us-east-2.amazonaws.com/WhitePC.jpeg"
-                    }
-                    width={25}
-                    height={25}
-                    layout="responsive"
-                  />
-                </Link>
-              </Card>
-            </Col>
-            <Col sm={12} md={12} lg={12} xl={12}>
-              <Card id="gh" type="button" className="btn btn-dark ">
-                <Link href="computer1">
-                  <a>
-                    <span>Our Design</span>
-                  </a>
-                </Link>
-              </Card>
-              <Card>
-                <Link href="computer1">
-                  <Image
-                    src={
-                      "https://secondbucketforcustompc.s3.us-east-2.amazonaws.com/BluePc.jpg"
-                    }
-                    width={25}
-                    height={25}
-                    layout="responsive"
-                  />
-                </Link>
-              </Card>
-            </Col>
-          </Row>
-        </Card>
-      </Card>
+          <Col sm={12} md={12} lg={12} xl={12} style={{ padding: "0" }}>
+            <Card id="gh" type="button" className="btn btn-dark ">
+              <Link href="computer1">
+                <a>
+                  <span>About Us</span>
+                </a>
+              </Link>
+            </Card>
+            <Card>
+              <Link href="computer1">
+                <Image
+                  src={
+                    "https://secondbucketforcustompc.s3.us-east-2.amazonaws.com/WhitePC.jpeg"
+                  }
+                  width={25}
+                  height={25}
+                  layout="responsive"
+                />
+              </Link>
+            </Card>
+          </Col>
+          <Col sm={12} md={12} lg={12} xl={12} style={{ padding: "0" }}>
+            <Card id="gh" type="button" className="btn btn-dark ">
+              <Link href="computer1">
+                <a>
+                  <span>Our Design</span>
+                </a>
+              </Link>
+            </Card>
+            <Card>
+              <Link href="computer1">
+                <Image
+                  src={
+                    "https://secondbucketforcustompc.s3.us-east-2.amazonaws.com/BluePc.jpg"
+                  }
+                  width={25}
+                  height={25}
+                  layout="responsive"
+                />
+              </Link>
+            </Card>
+          </Col>
+        </Row>
+      </Col>
     </div>
   );
 }
